536b9e4f603fb53043ba8e061707f71cb180c649
chenyue <chenyue@cycredit.com.cn>
2019-10-26 00:27:59 +0800

    根据时区调整时间

--- src/mixins/area-group.js
+++ src/mixins/area-group.js
@@ -1,11 +1,26 @@
 export default  {
     computed:{
+        timeDiff(){
+            let publishAreaCode = this.form.publishAreaCode;
+            if(!publishAreaCode){
+                return 0
+            }else{
+                let publishAreaCode = this.form.publishAreaCode,arr = this.$store.state.common.areaList.filter(item=>item.areaCode === publishAreaCode);
+                return parseInt(arr[0].timeZone);
+            }
+        },
         _planPubStartTime(){
             let val = this.form.planPubStartTime;
             if(!val){
                 return ''
             }else {
-                return `服务器对应的当地时间：${this.$dayjs(val).format('YYYY-MM-DD HH:mm a')}`
+                let timeDiff = this.timeDiff,_day = this.$dayjs(val);
+                if(timeDiff>0){
+                    _day = _day.add(timeDiff,'hour');
+                }else{
+                    _day = _day.subtract(Math.abs(timeDiff),'hour'); 
+                }
+                return `服务器对应的当地时间：${_day.format('YYYY-MM-DD HH:mm a')}`
             }
         },
         _planPubEndTime(){
@@ -13,7 +28,13 @@ export default  {
             if(!val){
                 return ''
             }else {
-                return `服务器对应的当地时间：${this.$dayjs(val).format('YYYY-MM-DD HH:mm a')}`
+                let timeDiff = this.timeDiff,_day = this.$dayjs(val);
+                if(timeDiff>0){
+                    _day = _day.add(timeDiff,'hour');
+                }else{
+                    _day = _day.subtract(Math.abs(timeDiff),'hour'); 
+                }
+                return `服务器对应的当地时间：${_day.format('YYYY-MM-DD HH:mm a')}`
             }
         },
         areaList(){
